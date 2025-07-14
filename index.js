cebe132cdba084efcca2ad44d80a5b8fbc530662
Akatsuki_No9 <psyper.no.9@gmail.com>
2025-07-14 22:06:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const works = [
     author: "高橋 次郎",
     title: "記憶の断片",
     Genre: "彫刻学科",
-    imageUrl: "Thumbnails/sample_4.png",
+    imageUrl: "works/2024/kioku-no-danpen/thumbnail.png",
     appUrl: "works/2024/kioku-no-danpen/"
   },
   {
